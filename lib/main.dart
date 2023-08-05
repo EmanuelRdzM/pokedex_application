7427ac03960fefa4d296247a8b6eb5dd0b67471a
EmanuelRdzM <j.emanuel.rdz.m@hotmail.com>
2023-08-05 09:43:26 -0600

    reset the view

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         providers: [
           BlocProvider(
             create: (context) =>
-                PokemonBloc()..add(PokemonPageRequest(page: 20)),
+                PokemonBloc()..add(PokemonPageRequest(page: 0)),
           )
         ],
         child: const PokedexView(),
